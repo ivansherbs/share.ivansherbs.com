3b97110f9ee5a4c5ba4e829cf34ec17ffcbe82e6
Gabriel Petrovay <gabriel.petrovay@swisscom.com>
2019-06-08 01:09:37 +0200

    Added shop article generation for the giver

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
+const debug = require('debug')('router:index')
+const config = require('config');
+
 var express = require('express');
 var router = express.Router();
 
-var tokens = require('../lib/tokens');
-var shop = require('../lib/shop');
+const tokens = require('../lib/tokens');
+const shop = require('../lib/shop');
 
-const SHOP_URL = 'https://freetrial.ivansherbs.com/detail/';
+const SHOP_URL = config.get('shops.share.shopUrlBase');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,41 +15,63 @@ router.get('/', function(req, res, next) {
 
     if (params.length === 1 && req.query[params[0]] === '') {
 
-        tokens.validateToken(params[0], (err, tokenDoc) => {
+        debug('Requested to share with code: %s', params[0]);
+
+        tokens.validateToken(params[0], (err, doc) => {
             if (err) {
+                debug('The requested share code %s has an issue: %s', params[0], err.code);
                 res.render('error', { error: { status: err.code } });
                 return;
             }
-            if (tokenDoc.email) {
-                res.redirect(tokenDoc.url || SHOP_URL);
+            if (doc.email) {
+                debug('Redirecting associated code (%s) to shop: %s', doc.token, doc.shopUrl);
+                res.redirect(doc.shopUrl || SHOP_URL);
                 return;
             }
+            debug('Capturing email for non-associated code: %s', doc.token);
             res.render('code');
         });
         return;
     }
+
+    debug('Requested to share with empty code');
+
     res.render('share');
 });
 
 /* POST email */
 router.post('/', function(req, res, next) {
+
     if (!req.body || !req.body.email || !req.body.token) {
         res.send({ code: 'INVALID_REQUEST' });
         return;
     }
-    tokens.associateEmail(req.body.token, req.body.email, (err, tokenDoc) => {
 
-        // TODO generate shop article
-        shop.addOneTimeArticle(req.body.email, (err, data) => {
+    var email = req.body.email;
+    var productId = shop.generateUniqueProductId(email);
+    debug('Generated product ID for the share shop: %s', productId);
+    var productUrl = SHOP_URL + productId;
+
+    var data = { email: email, shopUrl: productUrl };
+
+    debug('Associating code with data: %s', JSON.stringify(data));
+
+    tokens.associateData(req.body.token, data, (err, doc) => {
+
+        debug('Generating product in the share shop');
+
+        shop.generateProduct(productId, email, (err, data) => {
+
             if (err) {
                 console.error(err);
                 res.send({ code: 'CONTACT_SUPPORT' });
                 return;
             }
 
-            res.send({ code: 'OK', url: SHOP_URL + data.id });
+            res.send({ code: 'OK', url: productUrl });
         });
     });
 });
+
 
 module.exports = router;
